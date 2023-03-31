5edcd098aa1036757aa5fc4bdfa149d692b18762
happygetgit <sk39happy@gmail.com>
2023-03-31 21:24:23 +0530

    added first name validation, also a scenarion in spec with invalid signup details

--- spec/features/users/signing_users_up_spec.rb
+++ spec/features/users/signing_users_up_spec.rb
@@ -19,4 +19,22 @@ RSpec.feature "User signup form" do
         visit "/"
         expect(page).to have_content("John Doe")
     end
+
+    scenario "user signup with invalid details" do
+        visit "/"
+
+        click_link("Signup")
+
+        fill_in "First name", with: ""
+        fill_in "Last name", with: ""
+        fill_in "Email", with: ""
+        fill_in "Password", with: ""
+        fill_in "Password confirmation", with: ""
+
+        click_button "Sign up"
+
+        expect(page).to have_content("First name can't be blank")
+        expect(page).to have_content("Last name can't be blank")
+        
+    end
 end
